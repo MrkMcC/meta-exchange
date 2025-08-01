feaef573546a11498e164b31030b2c947c690122
Mark McCormack <Mark.McCormack@adesso.de>
2025-08-01 11:13:46 +0200

    Added Swagger documentation

--- MetaExchangeApi/Program.cs
+++ MetaExchangeApi/Program.cs
@@ -1,15 +1,31 @@
 using MetaExchange;
-using MetaExchange.Common.Enum;
 using MetaExchangeApi;
+using Swashbuckle.AspNetCore.Annotations;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => options.EnableAnnotations());
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Meta Exchange API");
+        options.RoutePrefix = string.Empty;
+    });
+app.MapOpenApi();
 
 var exchangeApiService = new MetaExchangeApiService(new ExchangeService());
 
-app.MapGet("/BTC/{type}/{amount}", IResult (string type, decimal amount) =>
+app.MapGet("/BTC/{type}/{amount}", IResult (
+    [SwaggerParameter("The type of transaction you would like to perform. 'Buy' if you want to buy BTC, 'Sell' if you want to sell them")] string type,
+    [SwaggerParameter("The amount of BTC you want to buy or sell")] decimal amount) =>
 {
     return exchangeApiService.GetBestExecutionPlan(type, amount);
-});
+})
+.WithDescription("Finds the best price to sell or buy a given amount of BTC and returns a set of orders to execute against the given exchanges.")
+.WithOpenApi();
 
 app.Run();
